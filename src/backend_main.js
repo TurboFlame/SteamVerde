5b910944c005d152b678e3800ef2fc201fbc8a3f
Gilbertop0108 <gpiedra@fi.uba.ar>
2024-12-06 17:53:22 -0300

    Agregar directorio src

---
+++ src/backend_main.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/favicon.ico', (req, res) => {
+    res.status(204).send();
+});
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('SteamVerde')
+})
+
+app.listen(port, () => {
+    console.log(`puerto numero ${port}`)
+})
+
