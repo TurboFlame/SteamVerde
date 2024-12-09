f6bee4597b06442c8223f0e430e8d7a84dc49dc3
Gilbertop0108 <gpiedra@fi.uba.ar>
2024-12-09 00:42:25 -0300

    endpoints de obtener por id, y agregar usuario

--- Backend/src/backend_main.js
+++ Backend/src/backend_main.js
@@ -47,3 +47,63 @@ app.get('/api/v1/desarrolladoras', async (req, res) =>{
     
     res.json(desarrolladoras)
 })
+
+app.get('/api/v1/usuarios/:id', async (req, res) =>{
+    const usuario = await prisma.usuario.findUnique({
+        where: {
+            id: parseInt(req.params.id)
+        }
+    })
+
+    if (usuario === null){
+        res.sendStatus(404)
+        return
+    }
+
+    res.json(usuario)
+})
+
+app.get('/api/v1/juegos/:id', async (req, res) =>{
+    const juego = await prisma.juego.findUnique({
+        where: {
+            id: parseInt(req.params.id)
+        }
+    })
+
+    if  (juego === null){
+        res.sendStatus(404)
+        return
+    }
+
+    res.json(juego)
+})
+
+app.get('/api/v1/desarrolladoras/:id', async (req, res) =>{
+    const desarrolladora = await prisma.desarrolladora.findUnique({
+        where: {
+            id: parseInt(req.params.id)
+        }
+    })
+
+    if (desarrolladora === null){
+        res.sendStatus(404)
+        return
+    }
+
+    res.json(desarrolladora)
+})
+
+app.post('/api/v1/usuarios', async (req, res) =>{
+    const usuario = await prisma.usuario.create({
+        data: {
+            nombre: req.body.nombre,
+            tipo_consola: req.body.tipo_consola,
+            juego_favorito: req.body.juego_favorito,
+            lista_deseados: undefined,
+            carrito: undefined,
+            dinero: parseFloat(req.body.dinero) ?? 0
+        }
+    })
+
+    res.status(201).send(usuario)
+})
