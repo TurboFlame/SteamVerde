f280c55e60a609d7e2a575f31ea90da955909865
TurboFlame <agusnahuel3@outlook.es>
2024-12-15 13:05:42 -0300

    Arreglada funcionalidad del checkbox en la pagina de registro. Agregado href a pagina de ingreso

--- Frontend/register.js
+++ Frontend/register.js
@@ -1,4 +1,4 @@
-//modal
+
 const botonAbrir=document.getElementById("boton-terminos");
 const modal=document.getElementById("modal-terminos");
 const modalFondo=document.getElementById("modal-fondo-terminos");
@@ -6,19 +6,20 @@ const cerrarModal=document.getElementById("modal-cerrar-boton");
 const checkTerminos=document.getElementById("check-terminos");
 const botonRegistro=document.getElementById("boton-registro");
 botonAbrir.addEventListener("click",()=>{
-    modal.style.display="none";
     modalFondo.style.display="block";
     modal.style.display="block";
 })
 cerrarModal.addEventListener("click",()=>{
     modal.style.display="none";
     modalFondo.style.display="block";
-    modal.style.display="none";
+
 })
-checkTerminos.addEventListener("change",()=>{
+checkTerminos.addEventListener("change", () => {
     if (checkTerminos.checked) {
-        botonRegistro.removeAttribute("disabled");
-    }else{
-        botonRegistro.setAttribute("disabled","");
+        botonRegistro.disabled=false;
+        botonRegistro.classList.remove("is-disabled");
+    } else {
+        botonRegistro.disabled=true;
+        botonRegistro.classList.add("is-disabled");
     }
-})
+});
