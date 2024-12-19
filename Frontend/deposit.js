e39a39ae12848f6c016d586dd588900f41f31bbd
TurboFlame <agusnahuel3@outlook.es>
2024-12-19 11:23:33 -0300

    Completada funcionalidad de integracion con Backend. Agregado texto de confirmacion de transaccion

--- Frontend/deposit.js
+++ Frontend/deposit.js
@@ -6,6 +6,7 @@ const botonDepositar= document.getElementById("boton-depositar");
 
 
 botonDepositar.addEventListener("click", function(){
+    event.preventDefault()
 
     const usuario = JSON.parse(sessionStorage.getItem('usuario'));
     usuario.dinero+=parseInt(inputMonto.value);
@@ -24,7 +25,7 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 async function depositar(dineroNuevo,idUsuario){
 
-    fetch(`http://localhost:3000/api/v1/usuarios/${idUsuario}`, {
+    const respuesta = fetch(`http://localhost:3000/api/v1/usuarios/${idUsuario}`, {
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json',
@@ -36,13 +37,26 @@ async function depositar(dineroNuevo,idUsuario){
         .then(response => {
             if (response.status === 200) {
                 console.log("Monto depositado con exito")
+                mensajeDepositoValidacionValido()
+            }else if (response.status === 404){
+                console.log(response)
+                mensajeDepositoValidacionInvalido("No inicio sesion de forma valida")
+            }else {
+                mensajeDepositoValidacionInvalido("Error durante transaccion")
             }
         })
 
-
-
-
-
-
-
+}
+const msgValidacionDeposito = document.getElementById("mensaje-validacion-deposito");
+function mensajeDepositoValidacionValido(){
+    msgValidacionDeposito.classList.remove("is-hidden");
+    msgValidacionDeposito.classList.remove("is-danger");
+    msgValidacionDeposito.classList.add("is-primary");
+    msgValidacionDeposito.textContent="Transaccion completada!"
+}
+function mensajeDepositoValidacionInvalido(cadena){
+    msgValidacionDeposito.classList.remove("is-hidden");
+    msgValidacionDeposito.classList.remove("is-primary");
+    msgValidacionDeposito.classList.add("is-danger");
+    msgValidacionDeposito.textContent=cadena
 }
